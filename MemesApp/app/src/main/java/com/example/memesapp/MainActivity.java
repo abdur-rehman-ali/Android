16079e216261c2a46935841e4ccb4e2cd15352bd
abdur-rehman-ali <alijoyia911@gmail.com>
2021-07-01 02:22:36 +0500

    Meme sharing app made

--- MemesApp/app/src/main/java/com/example/memesapp/MainActivity.java
+++ MemesApp/app/src/main/java/com/example/memesapp/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.memesapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.android.volley.Request.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String imageURL = null;
     private ImageView image;
     private Button sharebtn;
-    private Button nextbtn;
+    private Button nextBtn;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,30 +47,90 @@ public class MainActivity extends AppCompatActivity {
         //Targeting views
         image = findViewById(R.id.imageView);
         sharebtn = findViewById(R.id.sharebtn);
-        nextbtn = findViewById(R.id.nextbtn);
+        nextBtn = findViewById(R.id.nextbtn);
+        progressBar = findViewById(R.id.progressBar);
 
-
-        //Adding click events
+        //Adding click listener
         sharebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                shareMeme();
+                shareMEME();
             }
         });
 
-        nextbtn.setOnClickListener(new View.OnClickListener() {
+        nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nextMeme();
+                nextMEME();
             }
         });
+
+        loadRequest();
+
+
     }
 
-    private void nextMeme() {
-        Toast.makeText(this, "Next btn clciked", Toast.LENGTH_SHORT).show();
+    private void nextMEME() {
+        loadRequest();
     }
 
-    private void shareMeme() {
-        Toast.makeText(this, "Share btn clicked", Toast.LENGTH_SHORT).show();
+    private void shareMEME() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, imageURL);
+        intent.setType("text/plain");
+        startActivity(intent);
+
+    }
+
+    private void loadRequest() {
+
+        progressBar.setVisibility(View.VISIBLE);
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://meme-api.herokuapp.com/gimme";
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            imageURL = response.getString("url");
+                            Glide.with(getApplicationContext())
+                                    .load(imageURL)
+                                    .listener(new RequestListener<Drawable>() {
+                                        @Override
+                                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                            progressBar.setVisibility(View.GONE);
+                                            return false;
+                                        }
+
+                                        @Override
+                                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                            progressBar.setVisibility(View.GONE);
+                                            return false;
+                                        }
+                                    })
+                                    .into(image);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error" + error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+// Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
+
     }
 }
